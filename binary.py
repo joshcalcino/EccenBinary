a1540a8bd8242694d88e1eaaf5cb18c95938091c
Josh <josh.calcino@gmail.com>
2018-09-17 20:31:35 +1000

    fix

--- binary.py
+++ binary.py
@@ -3,6 +3,15 @@ from glob import glob
 import os
 
 
+def get_dump_files(directory):
+    dumpfiles = glob(directory)
+    new_dumpfiles = []
+    for dumpfile in dumpfiles:
+        if dumpfile[:-4].isdigit():
+            new_dumpfiles.append(dumpfile)
+    return new_dumpfiles
+
+
 eccentricity_filenames = ['eccen0', 'eccen01a', 'eccen02a', 'eccen03a', 'eccen04a', 'eccen05a', 'eccen06a']
 
 binary_ratio_filenames = ['br_0010', 'br_0050', 'br_0100']
@@ -10,7 +19,7 @@ binary_ratio_filenames = ['br_0010', 'br_0050', 'br_0100']
 for edir in eccentricity_filenames:
     for bdir in binary_ratio_filenames:
         cdir = os.path.join(edir, bdir, 'gas_only_hr')
-        dumpfiles = glob(cdir)
+        dumpfiles = get_dump_files(cdir)
         for dumpfile in dumpfiles:
             print(dumpfile)
             dump = pa(dumpfile)

193d73da2987fd638b04e4664119aa0425344997
Josh <josh.calcino@gmail.com>
2018-09-19 15:50:15 +1000

    fix

--- do_analysis.py
+++ do_analysis.py
@@ -9,12 +9,15 @@ binary_ratio_filenames = ['br_0010', 'br_0050', 'br_0100']
 for edir in eccentricity_filenames:
     for bdir in binary_ratio_filenames:
         cdir = os.path.join(edir, bdir, 'gas_only_hr/')
-        subprocess.check_output('cp phantomanalysis ' + cdir, stderr=subprocess.STDOUT,
+        output=subprocess.check_output('cp phantomanalysis ' + cdir, stderr=subprocess.STDOUT,
                                 universal_newlines=True, shell=True)
-        subprocess.check_output('cd ' + cdir, stderr=subprocess.STDOUT,
+        print(output)
+        output = subprocess.check_output('cd ' + cdir, stderr=subprocess.STDOUT,
                                 universal_newlines=True, shell=True)
-        subprocess.check_output('./phantomanalysis gas_only_hr_0*' + cdir, stderr=subprocess.STDOUT,
+        print(output)
+        output = subprocess.check_output('./phantomanalysis gas_only_hr_0*' + cdir, stderr=subprocess.STDOUT,
                                 universal_newlines=True, shell=True)
-        sink1 = numpy.genfromtxt('sinkpositions_1.dat')
-        sink2 = numpy.genfromtxt('sinkpositions_2.dat')
+        print(output)
+        sink1 = numpy.genfromtxt(cdir+'sinkpositions_1.dat')
+        sink2 = numpy.genfromtxt(cdir+'sinkpositions_2.dat')
         print(sink1)

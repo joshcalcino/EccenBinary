9c6c73fe408629e8ce4ab0599505262ebae2ee5b
Josh <josh.calcino@gmail.com>
2018-09-24 08:39:01 +1000

    debug

--- do_convolve.py
+++ do_convolve.py
@@ -1,6 +1,6 @@
 import subprocess
 import os
-import numpy
+
 
 eccentricity_filenames = ['eccen0', 'eccen01a', 'eccen02a', 'eccen03a', 'eccen04a', 'eccen05a', 'eccen06a']
 
@@ -10,6 +10,11 @@ for edir in eccentricity_filenames:
     for bdir in binary_ratio_filenames:
         cdir = os.path.join(edir, bdir, 'gas_only_hr/', 'mcfost', 'aph')
         os.chdir(cdir)
+
+        output = subprocess.check_output('which python', stderr=subprocess.STDOUT,
+                                         universal_newlines=True, shell=True)
+        print(output)
+
         subprocess.check_output('python convolve.py --filename ' + cdir + '/data_1/RT.fits.gz' + ' --save ' +
                                 edir+'_'+bdir + '_aph.png', stderr=subprocess.STDOUT,
                                          universal_newlines=True, shell=True)

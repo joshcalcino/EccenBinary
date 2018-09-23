8bef22ec6d91f97c8107fb789b5c040f236e62aa
Josh <josh.calcino@gmail.com>
2018-09-24 08:30:44 +1000

    debug

---
+++ do_convolve.py
@@ -0,0 +1,25 @@
+import subprocess
+import os
+import numpy
+
+eccentricity_filenames = ['eccen0', 'eccen01a', 'eccen02a', 'eccen03a', 'eccen04a', 'eccen05a', 'eccen06a']
+
+binary_ratio_filenames = ['br_0010']
+
+for edir in eccentricity_filenames:
+    for bdir in binary_ratio_filenames:
+        cdir = os.path.join(edir, bdir, 'gas_only_hr/', 'mcfost', 'aph')
+        os.chdir(cdir)
+        subprocess.check_output('python colvolve.py --filename ' + cdir + 'data_1/RT.fitz.gz' + ' --save ' +
+                                edir+cdir + '_aph.png', stderr=subprocess.STDOUT,
+                                         universal_newlines=True, shell=True)
+
+        os.chdir('../../../../../')
+
+        cdir = os.path.join(edir, bdir, 'gas_only_hr/', 'mcfost', 'per')
+        os.chdir(cdir)
+        subprocess.check_output('python colvolve.py --filename ' + cdir + 'data_1/RT.fitz.gz' + ' --save ' +
+                                edir+cdir + '_per.png', stderr=subprocess.STDOUT,
+                                         universal_newlines=True, shell=True)
+
+        os.chdir('../../../../../')

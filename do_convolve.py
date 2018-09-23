79564f74e082bea0ebd486f63e020731553bf16e
Josh <josh.calcino@gmail.com>
2018-09-24 08:34:48 +1000

    debug

--- do_convolve.py
+++ do_convolve.py
@@ -10,16 +10,16 @@ for edir in eccentricity_filenames:
     for bdir in binary_ratio_filenames:
         cdir = os.path.join(edir, bdir, 'gas_only_hr/', 'mcfost', 'aph')
         os.chdir(cdir)
-        subprocess.check_output('python convolve.py --filename ' + cdir + 'data_1/RT.fitz.gz' + ' --save ' +
-                                edir+cdir + '_aph.png', stderr=subprocess.STDOUT,
+        subprocess.check_output('python convolve.py --filename ' + cdir + '/data_1/RT.fitz.gz' + ' --save ' +
+                                edir+'_'+bdir + '_aph.png', stderr=subprocess.STDOUT,
                                          universal_newlines=True, shell=True)
 
         os.chdir('../../../../../')
 
         cdir = os.path.join(edir, bdir, 'gas_only_hr/', 'mcfost', 'per')
         os.chdir(cdir)
-        subprocess.check_output('python convolve.py --filename ' + cdir + 'data_1/RT.fitz.gz' + ' --save ' +
-                                edir+cdir + '_per.png', stderr=subprocess.STDOUT,
+        subprocess.check_output('python convolve.py --filename ' + cdir + '/data_1/RT.fitz.gz' + ' --save ' +
+                                edir+'_'+bdir + '_per.png', stderr=subprocess.STDOUT,
                                          universal_newlines=True, shell=True)
 
         os.chdir('../../../../../')
